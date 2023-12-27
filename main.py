8a4f21b6e841a8dc235b8d2f934694c078d408dc
kumdo0327 <kumdo0327@naver.com>
2023-12-27 15:00:59 +0900

    update

---
+++ main.py
@@ -0,0 +1,37 @@
+import unittest
+import subprocess
+import sys
+
+global_counter = 1
+
+def format_testcase(input_string):
+    parts = input_string.split(" (")
+    test_method = parts[0]
+    test_class = parts[1].rstrip(")")
+    transformed_string = f"test.{test_class}.{test_method}"
+    return transformed_string
+
+def subcall(suite):
+    if hasattr(suite, '__iter__'):
+        for x in suite:
+            subcall(x)
+    else:
+        global global_counter
+
+        testcase = format_testcase(str(suite))
+        subprocess.call(['coverage', 'run', '-m', 'unittest', '-q', testcase])
+        subprocess.call(['coverage', 'json', '-o', f'coverage/{global_counter}/summary.json', '--omit=test/*.py'])
+
+        result = suite.run()
+        with open(f'coverage/{global_counter}/{global_counter}.output', 'w') as f:
+            for _, traceback in result.errors:
+                f.write(traceback)
+        with open(f'coverage/{global_counter}/{global_counter}.test', 'w') as f:
+            f.write('passed' if result.wasSuccessful() else 'failed')
+
+        global_counter += 1
+
+
+
+if __name__ == '__main__':
+    subcall(unittest.defaultTestLoader.discover(sys.argv[1]))
