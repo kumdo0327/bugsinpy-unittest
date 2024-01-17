dc150b8adcd727f43da9f209527b8aa9a9287160
kumdo0327 <kumdo0327@naver.com>
2024-01-17 11:02:53 +0900

    update

--- main.py
+++ main.py
@@ -63,8 +63,9 @@ def main():
     if omission.endswith(','):
         omission = omission[:-1]
 
+    test_id = runUnittest()[0][0]
     print('\n>\n>\n>\n>\n>')
-    print(runUnittest()[0][0])
+    print(test_id)
     for test_id, report in runUnittest():
         runCoverage(test_id, report, omission)
 
